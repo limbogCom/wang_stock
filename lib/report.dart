af8a8828bd51808ce447465e0e2f3b0e630c9369
limbogPC <limbog.com@gmail.com>
2020-04-26 20:58:13 +0700

    add func for get item form DB

--- lib/report.dart
+++ lib/report.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wangstock/count_stock_model.dart';
 
 class ReportPage extends StatefulWidget {
   @override
@@ -8,8 +11,101 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPageState extends State<ReportPage> {
+
+  ScrollController _scrollController = new ScrollController();
+
+  //Product product;
+  List <CountStock>productAll = [];
+  bool isLoading = true;
+  int perPage = 30;
+  String act = "Report";
+
+  getCountStockProduct() async{
+
+    final res = await http.get('https://wangpharma.com/API/checkStockProduct.php?PerPage=$perPage&act=$act');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+        isLoading = false;
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((products) => productAll.add(CountStock.fromJson(products)));
+        perPage = perPage + 30;
+
+        print(productAll);
+        print(perPage);
+
+        return productAll;
+
+      });
+
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCountStockProduct();
+
+    _scrollController.addListener((){
+      //print(_scrollController.position.pixels);
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        getCountStockProduct();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: isLoading ? CircularProgressIndicator()
+          :ListView.separated(
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        controller: _scrollController,
+        itemBuilder: (context, int index){
+          return ListTile(
+            contentPadding: EdgeInsets.fromLTRB(10, 1, 10, 1),
+            onTap: (){
+              /*Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ReportDetailPage(receiveProducts: productAll[index])));*/
+            },
+            leading: Image.network('https://www.wangpharma.com/cms/product/${productAll[index].countStockProdPic}', fit: BoxFit.cover, width: 70, height: 70,),
+            title: Text('${productAll[index].countStockProdName}', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('${productAll[index].countStockProdCode}'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('${productAll[index].countStockBalance} : ${productAll[index].countStockProdUnit1}', style: TextStyle(color: Colors.red),),
+                    Text('พนง.เปิด ${productAll[index].countStockEmpAdd}', style: TextStyle(color: Colors.green),),
+                  ],
+                ),
+                Text('เปิดงาน ${productAll[index].countStockDateAdd}', style: TextStyle(color: Colors.blueGrey),),
+                //productAll[index].recevicProductUnitNew == null
+                    //? Text('หน่วยย่อย : ${productAll[index].recevicTCqtySub} ${productAll[index].recevicProductUnit}', style: TextStyle(color: Colors.lightBlue))
+                    //: Text('หน่วยย่อย : ${productAll[index].recevicTCqtySub} ${productAll[index].recevicProductUnitNew}', style: TextStyle(color: Colors.lightBlue)),
+              ],
+            ),
+
+            /*trailing: IconButton(
+                icon: Icon(Icons.view_list, size: 40,),
+                onPressed: (){
+                  //addToOrderFast(productAll[index]);
+                }
+            ),*/
+          );
+        },
+        itemCount: productAll != null ? productAll.length : 0,
+      ),
+    );
   }
 }
